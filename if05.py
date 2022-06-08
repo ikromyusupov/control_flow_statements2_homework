27dc7fce30eae1b8f920b95e015251468fd5bb4d
Ikrom <ikromyusupov09@gmail.com>
2022-06-08 14:34:15 +0500

    max number of five-digit number

--- if05.py
+++ if05.py
@@ -6,4 +6,29 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+
+    x1 = n % 10
+    n //= 10
+
+    x2 = n % 10
+    n //= 10
+
+    x3 = n % 10
+    n //= 10
+
+    x4 = n % 10
+    n //= 10
+
+    x5 = n % 10
+    n //= 10
+
+    mx = x1
+    if mx < x2:
+        mx = x2
+    if mx < x3:
+        mx = x3
+    if mx < x4:
+        mx = x4
+    if mx < x5:
+        mx = x5
+    return mx
